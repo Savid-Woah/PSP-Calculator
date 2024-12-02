f0ae0f34436f143549d387a86e606ef10c1e9495
Savid-Woah <savidoficial09@gmail.com>
2024-12-02 13:55:11 -0500

    Added - Menu to Main Method

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.Scanner;
+
+import static org.example.MathService.operate;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println(" ---------------------- ");
+        System.out.println(" ---------------------- ");
+
+        System.out.println("1. Sumar ");
+        System.out.println("2. Restar ");
+        System.out.println("3. Multiplicar ");
+        System.out.println("4. Dividir ");
+
+        System.out.print("Ingrese el número de la operación que desea ejecutar: ");
+        Integer optionSelected = scanner.nextInt();
+
+        MathOperation mathOperation = MathService.options.get(optionSelected);
+        if(mathOperation == null) throw new RuntimeException("Operación no válida");
+
+        System.out.print("Ingrese el primer número: ");
+        Double firstNumber = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo número: ");
+        Double secondNumber = scanner.nextDouble();
+
+        System.out.println(" ---------------------- ");
+        System.out.println(" ---------------------- ");
+
+        Double result = operate(firstNumber, secondNumber, mathOperation);
+
+        System.out.print("El resultado es: " + result);
     }
 }
